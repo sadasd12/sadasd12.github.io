c50f01a0e0a252fa11f52a893c995c5db7f74792
sadasd12 <sadasd12@outlook.com>
2015-11-30 23:41:45 +0800

    Update tiao.js

--- 66691f2f/tiao.js
+++ 66691f2f/tiao.js
@@ -1,2 +1,2 @@
- window.open ("http://www.749777.com/#cai/qe2z"); 
- window.location.href="http://www.749777.com/#cai/qe2z"; 
+ window.open("http://556.io/#cai"); 
+ window.location.href="http://556.io/#cai"; 
