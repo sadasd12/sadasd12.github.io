73e2529707c1bc811d3d93c2dd527023b2237cc6
sadasd12 <sadasd12@outlook.com>
2015-12-11 16:48:31 +0800

    Update tiao.js

--- hm00z/tiao.js
+++ hm00z/tiao.js
@@ -1,2 +1,3 @@
- window.open ("http://556.io/"); 
- window.location.href="http://556.io/"; 
+window.open ("http://www.551398.com/#免费公开一码"); 
+window.location.href="http://www.551398.com/#免费公开一码"; 
+
