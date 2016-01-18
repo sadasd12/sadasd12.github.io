ddacb0ce07451e05415052ce0dae49e301997674
sadasd12 <sadasd12@outlook.com>
2016-01-18 21:46:57 +0800

    Update tiao.js

--- hm5z/tiao.js
+++ hm5z/tiao.js
@@ -1,2 +1,3 @@
- window.open ("http://www.xrz6.com/#2016lh");
- window.location.href="http://www.xrz6.com/#2016lh";
+ window.open ("http://www.xrz6.com/index.htm#2016lh");
+ 
+ window.location.href="http://www.xrz6.com/index.htm#2016lh";
